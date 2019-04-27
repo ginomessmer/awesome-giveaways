10f43dfc4ac56696606a05431946fb6142c3b5b9
Thomas Pentenrieder <pentenrieder@hotmail.com>
2019-04-27 16:38:53 +0200

    Update privacy-policy-page.tsx

--- AwesomeGiveaways/ClientApp/src/features/privacy/privacy-policy-page.tsx
+++ AwesomeGiveaways/ClientApp/src/features/privacy/privacy-policy-page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const PrivacyPolicyPage = () => (
-	<div>
-
+    <div>
+        <h1>Huh, we didn't expect anyone to actually look at this...</h1>
+        <img src="https://media.giphy.com/media/xUOxeRVBTkYT2yOC5y/giphy.gif" />
+        <p>Well, as you're already here...</p>
+        <p>We will store the data in Azure (surprise) and will delete everything right after the raffle has concluded.</p>
+        <p>By submitting your data you agree to these "terms". Good luck!</p>
 	</div>
 )
 
