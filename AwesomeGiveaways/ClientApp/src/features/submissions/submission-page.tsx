272ed98c2820e22f5dcd64392aa07e5279c7c06a
Gino Messmer <git@ginomessmer.me>
2019-04-27 11:56:00 +0200

    Add submission form

--- AwesomeGiveaways/ClientApp/src/features/submissions/submission-page.tsx
+++ AwesomeGiveaways/ClientApp/src/features/submissions/submission-page.tsx
@@ -3,8 +3,33 @@ import React from 'react'
 const SubmissionPage = () => {
 	return (
 		<div>
-			Hello world
+			<h1 className="title">Take part</h1>
+			<SubmissionForm />
 		</div>
+	)
+}
+
+const SubmissionForm = () => {
+	return (
+		<form>
+			<div className="field">
+				<label className="label">Your Name</label>
+				<div className="control">
+					<input className="input" type="text" />
+				</div>
+			</div>
+			<div className="field">
+				<label className="label">Your Email Address</label>
+				<div className="control">
+					<input className="input" type="email" />
+				</div>
+			</div>
+			<div>
+				<p><small>By submitting this form, you agree to our privacy policy.</small></p>
+
+				<button className="button is-link">Submit</button>
+			</div>
+		</form>
 	)
 }
 
