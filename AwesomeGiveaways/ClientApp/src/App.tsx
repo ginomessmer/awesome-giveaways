da78057d7e437ceec3a1bf85417c237a2fe0234d
Gino Messmer <git@ginomessmer.me>
2019-04-27 13:31:47 +0200

    Add dashboard page layout

--- AwesomeGiveaways/ClientApp/src/App.tsx
+++ AwesomeGiveaways/ClientApp/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { SubmissionPage } from './features/submissions/submission-page';
+import { DashboardPage } from './features/dashboard/dashboard-page';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={SubmissionPage} />
+      <Route path="/dashboard" component={DashboardPage} />
     </BrowserRouter>
   );
 }
---
+++ AwesomeGiveaways/ClientApp/src/features/dashboard/dashboard-page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const DashboardPage = () => {
+	return (
+		<section className="hero is-fullheight">
+			<div className="hero-body">
+				<div className="container has-text-centered">
+					<h1 className="title is-size-1">10</h1>
+					<h2 className="subtitle">People signed up</h2>
+				</div>
+			</div>
+		</section>
+	)
+}
